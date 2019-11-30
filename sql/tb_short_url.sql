a5b877bde3fb208cde8e6a0bb83c6ba6009cbade
zheng_weilu@163.com <zheng_weilu@163.com>
2019-11-30 22:42:11 +0800

    sql

---
+++ sql/tb_short_url.sql
@@ -0,0 +1,39 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50717
+ Source Host           : localhost:3306
+ Source Schema         : db_shorturl
+
+ Target Server Type    : MySQL
+ Target Server Version : 50717
+ File Encoding         : 65001
+
+ Date: 26/11/2019 09:54:59
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tb_short_url
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_short_url`;
+CREATE TABLE `tb_short_url` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `long_url` varchar(240) NOT NULL COMMENT '长码',
+  `short_url` varchar(120) NOT NULL COMMENT '短码',
+  `type` int(11) NOT NULL DEFAULT '0' COMMENT '类型',
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '状态',
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_at` datetime DEFAULT NULL COMMENT '更改时间',
+  `deleted` int(11) DEFAULT '0' COMMENT '0-未删除 1已删除',
+  `version` int(11) DEFAULT '0' COMMENT '版本号',
+  PRIMARY KEY (`id`),
+  KEY `long_url` (`long_url`) USING BTREE,
+  KEY `short_url` (`short_url`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8 COMMENT='短网址（Short URL）映射关系';
+
+SET FOREIGN_KEY_CHECKS = 1;
